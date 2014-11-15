0af0f33ec22a11aceb854c2dcef82822e46c0fdb
Neil Howie <neil.howie@yahoo.co.uk>
2014-11-15 20:58:35 +0000

    works in IOS now :-)

--- app/scripts/dronihil.js
+++ app/scripts/dronihil.js
@@ -21,21 +21,49 @@ function feedvals(component, xval, yval) {
   var a = 0.5 + ((Math.atan(ypos/xpos)/Math.PI));
 
   // console.log('x: ' + xval + ' y: ' + yval + 'Hue: ' + (a*359) + ' sat + ' + (0.6 + d/3) + ' l: ' + (0.2 + d*0.7));
-  component.css('background', jQuery.Color({ hue: (a*359), saturation: (0.1 + d/5), lightness: 0.2 + d*0.7}));
+  component.css('background', jQuery.Color({ hue: (a*359), saturation: (0.1 + d/16), lightness: d*0.3}));
 }
 
 function bclick(component) {
   'use strict';
+  if (dronihil.started !== true) {
+    flock.enviro.shared.play();
+
+    var as = flock.enviro.shared.audioStrategy;
+    if (as.model.shouldInitIOS) {
+      var s = as.source;
+      (s.start || s.noteOn).call(as.source, 0);
+      (s.stop || s.noteOff).call(as.source, 0);
+      as.model.shouldInitIOS = false;
+    }
+    dronihil.started = true;
+  }
+  if (component.clicked) { return; }
+  component.clicked = true;
   $('.drnbt').removeClass('btnactive');
   component.addClass('btnactive');
   var id = component.attr('id');
   dronihil.recording = id;
-  $('body').mouseup(function() { $('body').off('mousemove'); component.removeClass('btnactive'); dronihil.recording = undefined; });
+  $('body').on("mouseup mouseleave touchend touchcancel", function() {
+    $('body').off('mousemove touchmove');
+    component.clicked = false;
+    component.removeClass('btnactive');
+    dronihil.recording = undefined;
+  });
   var maxw = $('body').width();
   var maxh = $('body').height();
 
-  $('body').mousemove(function(event) {
-    dronihil.recordVal = { xval: (maxw - event.pageX)/maxw, yval: event.pageY/maxh };
+  $('body').on ("mousemove touchmove", function(event) {
+    var x, y;
+    if (event.originalEvent !== undefined) {
+      x = event.originalEvent.touches[0].pageX;
+      y = event.originalEvent.touches[0].pageY;
+    } else {
+      x = event.pageX;
+      y = event.pageY;
+    }
+
+    dronihil.recordVal = { xval: (maxw - x)/maxw, yval: y/maxh };
   });
 
 }
@@ -80,7 +108,12 @@ function procAuto() {
   dronihil.place = {};
   dronihil.addAuto = function( args ) {
     var cmp = args.component;
-    cmp.mousedown( function() { bclick(cmp); });
+    //cmp.mousedown( function() { bclick(cmp); });
+    cmp.on("mousedown touchstart", function(ev) {
+      ev.preventDefault();
+      ev.stopImmediatePropagation();
+      bclick(cmp);
+    });
     args.xrange = args.xmax - args.xmin;
     args.yrange = args.ymax - args.ymin;
     dronihil.autoData[cmp.attr('id')] = args;
@@ -186,7 +219,7 @@ function procAuto() {
     // If you're on iOS, you will need to call in a listener for
     // some kind of user input action, such a button click or touch handler.
     // This is because iOS will only play sound if the user initiated it.
-    flock.enviro.shared.play();
+    // flock.enviro.shared.play();
     setInterval(function() { procAuto(); }, 100 );
   };
 
